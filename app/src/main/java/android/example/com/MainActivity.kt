f70902ff5cbca91b5d6da6cbb2c7e26921d7c1f1
David PEREZ <David.PEREZ@ingenico.com>
2021-02-18 15:06:35 -0600

    Data binding branch.

--- app/src/main/java/android/example/com/MainActivity.kt
+++ app/src/main/java/android/example/com/MainActivity.kt
@@ -1,23 +1,30 @@
 package android.example.com
 
+import android.example.com.databinding.ActivityMainBinding
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.databinding.DataBindingUtil
+
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setListeners()
     }
 
     private fun setListeners() {
         val clickableViews: List<View> =
             listOf(
-                box_one_text, box_two_text, box_three_text,
-                box_four_text, box_five_text, cons_layout
+                binding.boxOneText, binding.boxTwoText, binding.boxThreeText,
+                binding.boxFourText, binding.boxFiveText, binding.consLayout
             )
 
         for (item in clickableViews) {
@@ -27,14 +34,25 @@ class MainActivity : AppCompatActivity() {
 
     private fun makeColored(view: View){
         when(view.id){
-            // Boxes using Color class colors for background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+//            // Boxes using Color class colors for background
+//            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+//            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+//
+//            // Boxes using Android color resources for background
+//            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_blue_light)
+//            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_blue_dark)
+//            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_blue_bright)
+//
+//            else -> view.setBackgroundColor(Color.LTGRAY)
+
+            //Boxes using Color class colors for background
+            binding.boxOneText.id -> view.setBackgroundColor(Color.DKGRAY)
+            binding.boxTwoText.id -> view.setBackgroundColor(Color.GRAY)
 
             // Boxes using Android color resources for background
-            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_blue_light)
-            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_blue_dark)
-            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_blue_bright)
+            binding.boxThreeText.id -> view.setBackgroundResource(android.R.color.holo_blue_light)
+            binding.boxFourText.id -> view.setBackgroundResource(android.R.color.holo_blue_dark)
+            binding.boxFiveText.id -> view.setBackgroundResource(android.R.color.holo_blue_bright)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
